e9b5a2c6a0e0c4df31c9929bc160f4b2e7dbe98c
ByungChul-Cha <lastsc1404@gmail.com>
2024-05-26 22:15:46 +0900

    upload

---
+++ flutter_application_1/lib/camera.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Camera extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text('카메라로 검색'),
+    ));
+  }
+}
---
+++ flutter_application_1/lib/community.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Community extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text('커뮤니티'),
+    ));
+  }
+}
